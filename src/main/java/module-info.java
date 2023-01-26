9c2be7acb62f077ba349503831f2758fb066f092
Thế Anh <theanh291102@gmail.com>
2023-01-26 11:18:10 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module vietphap.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens vietphap.gui to javafx.fxml;
+    exports vietphap.gui;
+}
---
+++ src/main/java/vietphap/gui/SceneController.java
@@ -0,0 +1,37 @@
+package vietphap.gui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchTomainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchTohelloView(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("helloView.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
